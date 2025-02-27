7bf5d32ddb4a60af5c2bf32dd27eaa9d509fbc6f
ioiohorihori <horiori@gmail.com>
2025-02-27 13:47:36 +0900

    change config

--- db/production.sql
+++ db/production.sql
@@ -1,2 +1,2 @@
-CREATE DATABASE IF NOT EXISTS `maichike_production` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE IF NOT EXISTS `test_rails_production` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
