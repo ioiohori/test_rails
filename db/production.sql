ab6dbc88b415456ba1e97de1df947ed84f22bd46
ioiohorihori <horiori@gmail.com>
2025-02-27 15:22:18 +0900

    change config

--- db/production.sql
+++ db/production.sql
@@ -1,2 +1,5 @@
 CREATE DATABASE IF NOT EXISTS `test_rails_production` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE IF NOT EXISTS `test_rails_production_cache` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE IF NOT EXISTS `test_rails_production_queue` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE IF NOT EXISTS `test_rails_production_cable` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
